78d7ba6a6120a3a799ba76335d80149c7eea8dfb
Ubuntu <ubuntu@ip-172-31-45-93.us-east-2.compute.internal>
2019-11-25 09:39:51 +0000

    created string.py and utils.py

---
+++ src/string.py
@@ -0,0 +1,15 @@
+import re
+
+def clean_comment(comment):
+    comment = comment.lower()
+    comment = re.sub(r'(https?://\S+)',r' ',comment) # Remove URL's
+    comment = re.sub(r'\n','>',comment) # Replace \n with a special character to denote linebreaks
+    comment = re.sub(r'(/r/\S+)',r' ',comment) # remove links to specific subreddits
+    comment = re.sub(r'[^A-Za-z0-9 >,\.!?\']', ' ', comment) # Keep only these characters
+    comment = re.sub(r'amp nbsp',' ', comment) 
+    comment = re.sub(r'\s([?.!",](?:\s|$))', r'\1', comment) # remove white space between text and interpunction
+    comment = re.sub('\s*([>])\s*', r'\1', comment) # remove space between line breaks
+    comment = re.sub(' +',' ',comment) # Remove multi-white space
+    comment = re.sub('>+','>',comment) # Remove multi-linebreaks
+    comment = comment.strip()
+    return comment
---
+++ src/utils.py
@@ -0,0 +1,9 @@
+import re
+
+def print2(comment):
+    print(re.sub(r'>',r'\n',comment))
+    
+def print2_list(comment_list):
+    for x in comment_list:
+        print2(x)
+        print('\n-------\n')
